f9a3e03e42df18f0ba7076ad715cc3759a92c931
juankparis <juan.k.paris@gmail.com>
2020-03-23 23:54:34 -0500

    x

--- src/scripts/lib/loadCSS.js
+++
@@ -1,8 +0,0 @@
-var loadCSS = function (url) {
-  var elem = document.createElement('link');
-  elem.rel = 'stylesheet';
-  elem.href = url;
-  document.head.appendChild(elem);
-}
-
-module.exports = loadCSS;
--- src/scripts/lib/loadHeadImg.js
+++
@@ -1,8 +0,0 @@
-var loadHeadImg = function (url) {
-  var elem = document.createElement('link');
-  elem.rel = 'shortcut icon';
-  elem.href = url;
-  document.head.appendChild(elem);
-}
-
-module.exports = loadHeadImg;
